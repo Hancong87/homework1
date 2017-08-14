3c5d11b3a42ba5d58b3af73674aca325bc6c8249
pollux <guanhancong@gmail.com>
2017-08-14 21:58:02 +0800

    homework travis

--- quz/quz.js
+++ quz/quz.js
@@ -9,6 +9,13 @@
  */
 function dcate(A, B) {
 	/** Fill in here **/
+	var elem = A, last;
+	while (elem) {
+		last = elem;
+		elem = elem.tail;
+	}
+	last.tail = B;
+	return A;
 }
 
 /**
@@ -24,4 +31,16 @@ function dcate(A, B) {
  */
 function sub(L, start, len) {
 	/** Fill in here **/
+	var res = [];
+	var elem = L;
+	var n = 0;
+	while (elem) {
+		if (n >= start && n <= start + len) {
+			res.push(elem.head);
+			if (n == start + len) break;
+		}
+		n++;
+		elem = elem.tail;
+	}
+	return List.list(res);
 }
